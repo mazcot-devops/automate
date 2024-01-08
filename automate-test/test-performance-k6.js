9d5754bc25c1f2ec036d989a8335b030ea855afa
mazcot-devops <natthwut.rao@gmail.com>
2024-01-08 14:11:05 +0700

    fix api

--- automate-test/test-performance-k6.js
+++ automate-test/test-performance-k6.js
@@ -1,10 +1,19 @@
 import http from 'k6/http';
-import { check } from 'k6';
+import { check, Trend } from 'k6';
+
+
+const durationRegister = new Trend('duration_register');
+const durationLogin = new Trend('duration_login');
+const durationUpdate = new Trend('duration_update');
+const durationDelete = new Trend('duration_delete');
 
 export let options = {
     stages: [
-        { duration: '30s', target: 10 }, 
-        { duration: '1m', target: 150 }, 
+        { duration: '20s', target: 10 }, 
+        { duration: '1m', target: 300 }, 
+        { duration: '2m', target: 500 },
+        { duration: '30s', target: 100 }, 
+        { duration: '30s', target: 50 }, 
         { duration: '30s', target: 0 }, 
     ],
 };
@@ -18,26 +27,38 @@ export default function () {
         },
     };
     
+ 
+    let startRegister = Date.now();
     let registerRes = http.post(`http://localhost:3000/register`, JSON.stringify({ username, password }), newUserParams);
+    durationRegister.add(Date.now() - startRegister);
     check(registerRes, {
         'is status 200': (r) => r.status === 200,
         'is user registered': (r) => r.body.indexOf('User registered successfully') !== -1,
     });
 
+    
+    let startLogin = Date.now();
     let loginRes = http.post(`http://localhost:3000/login`, JSON.stringify({ username, password }), newUserParams);
+    durationLogin.add(Date.now() - startLogin);
     check(loginRes, {
         'is status 200': (r) => r.status === 200,
         'is login successful': (r) => r.body.indexOf('Login successful') !== -1,
     });
 
+    
     let newPassword = `${password}_new`;
+    let startUpdate = Date.now();
     let updateRes = http.put(`http://localhost:3000/update`, JSON.stringify({ username, newPassword }), newUserParams);
+    durationUpdate.add(Date.now() - startUpdate);
     check(updateRes, {
         'is status 200': (r) => r.status === 200,
         'is password updated': (r) => r.body.indexOf('Password updated successfully') !== -1,
     });
 
+    
+    let startDelete = Date.now();
     let deleteRes = http.del(`http://localhost:3000/delete`, JSON.stringify({ username, password: newPassword }), newUserParams);
+    durationDelete.add(Date.now() - startDelete);
     check(deleteRes, {
         'is status 200': (r) => r.status === 200,
         'is user deleted': (r) => r.body.indexOf('User deleted successfully') !== -1,
